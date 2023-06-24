c1d6b46f069922f1a7ee18bd6abf6db0d92cb8dd
USII-004 <usmansmooth004@gmail.com>
2023-06-24 02:00:09 +0100

    minor changes to html

--- validateInput.js
+++ validateInput.js
@@ -86,45 +86,46 @@ selectBox.addEventListener("change", removeItem);
 
 function removeItem() {
   if (previousOption == "cleaning") {
-    document.getElementById("cleaning-checkbox-container").style.display = "block";    
+    document.getElementById("cleaning").style.display = "block";    
   }else if (previousOption == "laundry") {
-    document.getElementById("laundry-checkbox-container").style.display = "block";    
+    document.getElementById("laundry").style.display = "block";    
   } else if (previousOption == "dog-walking") {
-    document.getElementById("dog-walking-checkbox-container").style.display = "block";    
+    document.getElementById("dog-walking").style.display = "block";    
   }else if (previousOption == "outdoor-errands") {
-    document.getElementById("outdoor-errands-checkbox-container").style.display = "block";    
+    document.getElementById("outdoor-errands").style.display = "block";    
   }else if (previousOption == "baby-sitting") {
-    document.getElementById("baby-sitting-checkbox-container").style.display = "block";    
+    document.getElementById("baby-sitting").style.display = "block";    
   }else if (previousOption == "gardening") {
-    document.getElementById("gardening-checkbox-container").style.display = "block";    
+    document.getElementById("gardening").style.display = "block";    
   }else if (previousOption == "plumbering") {
-    document.getElementById("plumbering-checkbox-container").style.display = "block";    
+    document.getElementById("plumbering").style.display = "block";    
   }else if (previousOption == "home-appliance-repair") {
-    document.getElementById("home-appliance-repair-checkbox-container").style.display = "block";    
+    document.getElementById("home-appliance-repair").style.display = "block";    
   }
+
   // if statement to display the removed items in the checkbox
 
   selectedOption = selectBox.value;
   if (selectedOption == "cleaning") {
-    document.getElementById("cleaning-checkbox-container").style.display = "none";    
+    document.getElementById("cleaning").style.display = "none";    
   }else if (selectedOption == "laundry") {
-    document.getElementById("laundry-checkbox-container").style.display = "none";    
+    document.getElementById("laundry").style.display = "none";    
   } else if (selectedOption == "dog-walking") {
-    document.getElementById("dog-walking-checkbox-container").style.display = "none";    
+    document.getElementById("dog-walking").style.display = "none";    
   }else if (selectedOption == "outdoor-errands") {
-    document.getElementById("outdoor-errands-checkbox-container").style.display = "none";    
+    document.getElementById("outdoor-errands").style.display = "none";    
   }else if (selectedOption == "baby-sitting") {
-    document.getElementById("baby-sitting-checkbox-container").style.display = "none";    
+    document.getElementById("baby-sitting").style.display = "none";    
   }else if (selectedOption == "gardening") {
-    document.getElementById("gardening-checkbox-container").style.display = "none";    
+    document.getElementById("gardening").style.display = "none";    
   }else if (selectedOption == "plumbering") {
-    document.getElementById("plumbering-checkbox-container").style.display = "none";    
+    document.getElementById("plumbering").style.display = "none";    
   }else if (selectedOption == "home-appliance-repair") {
-    document.getElementById("home-appliance-repair-checkbox-container").style.display = "none";    
+    document.getElementById("home-appliance-repair").style.display = "none";    
   }
   previousOption = selectedOption;
 }
 
-//this code now works as expected as the removed items is returned wheever another item is
+//this code now works as expected as the removed items is returned whenever another item is
 // selected. However this are spagheti code as there must be a way to rewrite the code
 // and reduce the long  if else statements. 
